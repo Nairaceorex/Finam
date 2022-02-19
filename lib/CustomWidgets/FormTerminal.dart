98926da8a77d48403d4298142c07e3f0acacc969
Nairaceorex <wwwolf2002@gmail.com>
2022-02-20 00:23:01 +0300

    fix TerminalForm(design,validator,using Firebase); TableReport using Firebase

--- lib/CustomWidgets/FormTerminal.dart
+++ lib/CustomWidgets/FormTerminal.dart
@@ -1,5 +1,6 @@
 import 'package:banking/CustomWidgets/DropButtonTerminalAcc.dart';
 import 'package:banking/CustomWidgets/DropButtonTerminalOper.dart';
+import 'package:banking/CustomWidgets/TableReport.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -15,67 +16,71 @@ class FormTerminalState extends State {
   var _summary = 0;
 
   Stream<QuerySnapshot> _operationDrop =
-  FirebaseFirestore.instance.collection('Operation')
-      .snapshots();
+      FirebaseFirestore.instance.collection('Operation').snapshots();
   String? dropdownValueOper;
   var _oper;
 
-  Stream<QuerySnapshot> _accountDrop =
-  FirebaseFirestore.instance.collection('Account')
+  Stream<QuerySnapshot> _accountDrop = FirebaseFirestore.instance
+      .collection('Account')
       .where("user_uid", isEqualTo: "${FirebaseAuth.instance.currentUser!.uid}")
       .snapshots();
   String? dropdownValueAcc;
   var _acc;
 
   Widget buildDporOper() => Container(
-    child: StreamBuilder<QuerySnapshot>(
-        stream: _operationDrop,
-        builder: (BuildContext context,
-            AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('problem');
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text('Loading');
-          }
-          final data = snapshot.requireData;
+        child: StreamBuilder<QuerySnapshot>(
+            stream: _operationDrop,
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return Text('problem');
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Text('Loading');
+              }
+              final data = snapshot.requireData;
 
-          return DropdownButton<String>(
-            hint: Text('Oper'),
-            value: dropdownValueOper,
-            icon: const Icon(Icons.arrow_downward),
-            elevation: 16,
-            style: const TextStyle(color: Colors.deepPurple),
-            underline: Container(
-              height: 2,
-              color: Colors.deepPurpleAccent,
-            ),
-            onChanged: (String? newValue) {
-              _oper = newValue;
-              print(_oper);
-              setState(() {
-                dropdownValueOper = newValue!;
-              });
-            },
-            items: getDocDataOper(data, data.size, snapshot)
-                .map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
+              return DropdownButtonFormField<String>(
+                hint: Text('Oper'),
+                value: dropdownValueOper,
+                icon: const Icon(Icons.arrow_downward),
+                elevation: 16,
+                style: const TextStyle(color: Colors.deepPurple),
+                /*underline: Container(
+                  height: 2,
+                  color: Colors.deepPurpleAccent,
+                ),*/
+                onChanged: (String? newValue) {
+                  _oper = newValue;
+                  print(_oper);
+                  setState(() {
+                    dropdownValueOper = newValue!;
+                  });
+                },
+                validator: (newValue){
+                  if (newValue == null || newValue.isEmpty){
+                    return "Select operation";
+                  }
+                },
+                items: getDocDataOper(data, data.size, snapshot)
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
               );
-            }).toList(),
-          );
-        }),
-  );
-  List<String> getDocDataOper(data, g, snapshot) {
+            }),
+      );
 
+  List<String> getDocDataOper(data, g, snapshot) {
     var docs = snapshot.data.docs;
     /* var doc = docs[g-1];
     final acc = doc.data();*/
 
     List<String> wasd = <String>[];
 
-    for(int i=0; i < g; i++){
+    for (int i = 0; i < g; i++) {
       var doc = docs[i];
       final acc = doc.data();
       wasd.insert(i, acc['Type'].toString());
@@ -94,55 +99,60 @@ class FormTerminalState extends State {
   }
 
   Widget buildDporAcc() => Container(
-    child: StreamBuilder<QuerySnapshot>(
-        stream: _accountDrop,
-        builder: (BuildContext context,
-            AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('problem');
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text('Loading');
-          }
-          final data = snapshot.requireData;
+        child: StreamBuilder<QuerySnapshot>(
+            stream: _accountDrop,
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return Text('problem');
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Text('Loading');
+              }
+              final data = snapshot.requireData;
 
-          return DropdownButton<String>(
-            hint: Text('Acc'),
-            value: dropdownValueAcc,
-            icon: const Icon(Icons.arrow_downward),
-            elevation: 16,
-            style: const TextStyle(color: Colors.deepPurple),
-            underline: Container(
-              height: 2,
-              color: Colors.deepPurpleAccent,
-            ),
-            onChanged: (String? newValue) {
-              _acc = newValue;
+              return DropdownButtonFormField<String>(
+                hint: Text('Acc'),
+                value: dropdownValueAcc,
+                icon: const Icon(Icons.arrow_downward),
+                elevation: 16,
+                style: const TextStyle(color: Colors.deepPurple),
+                /*underline: Container(
+                  height: 2,
+                  color: Colors.deepPurpleAccent,
+                ),*/
+                onChanged: (String? newValue) {
+                  _acc = newValue;
 
-              print(_acc);
-              setState(() {
-                dropdownValueAcc = newValue!;
-              });
-            },
-            items: getDocDataAcc(data, data.size, snapshot)
-                .map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
+                  print(_acc);
+                  setState(() {
+                    dropdownValueAcc = newValue;
+                  });
+                },
+                validator: (newValue){
+                  if (newValue == null || newValue.isEmpty){
+                    return "Select account";
+                  }
+                },
+                items: getDocDataAcc(data, data.size, snapshot)
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
               );
-            }).toList(),
-          );
-        }),
-  );
-  List<String> getDocDataAcc(data, g, snapshot) {
+            }),
+      );
 
+  List<String> getDocDataAcc(data, g, snapshot) {
     var docs = snapshot.data.docs;
     /* var doc = docs[g-1];
     final acc = doc.data();*/
 
     List<String> wasd = <String>[];
 
-    for(int i=0; i < g; i++){
+    for (int i = 0; i < g; i++) {
       var doc = docs[i];
       final acc = doc.data();
       wasd.insert(i, acc['name'].toString());
@@ -165,46 +175,43 @@ class FormTerminalState extends State {
       body: Center(
         child: ListView(
           children: [
-        Container(
-        padding: EdgeInsets.all(10.0),
-          child: Form(
-              key: _formKey,
-              child: new Column(
-                children: <Widget>[
-                  new Text('Сумма', style: TextStyle(fontSize: 20.0),),
-                  new TextFormField(
-                    onChanged: (value){
-                      _summary = int.parse(value);
-                      print(_summary);
-                    },
-                      validator: (value){
-                        if (value!.isEmpty) return 'Пожалуйста введите число';
-                        String num = "[0-9]";
-                        RegExp regExp = new RegExp(num);
-                        if(regExp.hasMatch(value)) return null;
-                        return 'Используйте числа';
-                  }
-
-                  ),
-                  new SizedBox(height: 20.0),
-                  new Text('Операция', style: TextStyle(fontSize: 20.0),),
-
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: buildDporOper(),
-                  ),
-
-                  new SizedBox(height: 20.0),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: buildDporAcc(),
-                  ),
-
-
-                  new SizedBox(height: 20.0),
-
-                 _button("Add","name",_summary,_oper),
-                  /*ElevatedButton(onPressed: (){
+            Container(
+                padding: EdgeInsets.all(10.0),
+                child: Form(
+                    key: _formKey,
+                    child: new Column(
+                      children: <Widget>[
+                        new Text(
+                          'Сумма',
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                        new TextFormField(onChanged: (value) {
+                          _summary = int.parse(value);
+                          print(_summary);
+                        }, validator: (value) {
+                          if (value!.isEmpty) return 'Пожалуйста введите число';
+                          String num = "[0-9]";
+                          RegExp regExp = new RegExp(num);
+                          if (regExp.hasMatch(value)) return null;
+                          return 'Используйте числа';
+                        }),
+                        new SizedBox(height: 20.0),
+                        new Text(
+                          'Операция',
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: buildDporOper(),
+                        ),
+                        new SizedBox(height: 20.0),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: buildDporAcc(),
+                        ),
+                        new SizedBox(height: 20.0),
+                        _button("Add", _acc, _summary, _oper),
+                        /*ElevatedButton(onPressed: (){
                     if(_formKey.currentState!.validate())
                       ScaffoldMessenger.of(context).showSnackBar(
                           SnackBar(
@@ -219,35 +226,40 @@ class FormTerminalState extends State {
                       onPrimary: Colors.white, // foreground
                     ),
                   ),*/
-
-                ],
-              )
-          )
-      ),
+                      ],
+                    ))),
           ],
         ),
       ),
     );
-
   }
-  Widget _button(String text, var name, var sum, var oper){
-    CollectionReference _history = FirebaseFirestore.instance.collection('History');
+
+  Widget _button(String text, var name, var sum, var oper) {
+    CollectionReference _history =
+        FirebaseFirestore.instance.collection('History');
 
     return ElevatedButton(
-      onPressed: (){
+      onPressed: () {
         // Переходим к новому маршруту
-        if(_formKey.currentState!.validate()){
-          ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Счёт успешно добавлен'),
-                backgroundColor: Colors.green,
-              )
-          );
+        if (_formKey.currentState!.validate()) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('Операция успешно выполнена'),
+            backgroundColor: Colors.green,
+          ));
           print(name);
           print(sum);
-          _history.add({'name': name, 'summary': sum, "date_time": Timestamp.now(), 'user_uid':FirebaseAuth.instance.currentUser!.uid });
-
+          print(oper);
+          _history.add({
+            'account': name,
+            'summary': sum,
+            'operation': oper,
+            "date_time": Timestamp.now(),
+            'user_uid': FirebaseAuth.instance.currentUser!.uid
+          });
+          //Navigator.push(context, MaterialPageRoute(builder: (context) => TableReport()));
         }
+        //_formKey.currentState!.reset();
+
       },
       child: Text(text),
       style: ElevatedButton.styleFrom(
--- lib/CustomWidgets/TableReport.dart
+++ lib/CustomWidgets/TableReport.dart
@@ -1,13 +1,71 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TableReport extends StatefulWidget{
   @override
   TableReportState createState() => TableReportState();
 }
 class TableReportState extends State<TableReport>{
+  final Stream<QuerySnapshot> account =
+  FirebaseFirestore.instance.collection('History').where("user_uid", isEqualTo: "${FirebaseAuth.instance.currentUser!.uid}").snapshots();
+
   @override
   Widget build(BuildContext context) {
-    return DataTable(
+    return Container(
+      child: StreamBuilder<QuerySnapshot>(
+          stream: account,
+          builder: (
+              BuildContext context,
+              AsyncSnapshot<QuerySnapshot> snapshot){
+            if(snapshot.hasError) {
+              return Text('problem');
+            }
+            if (snapshot.connectionState == ConnectionState.waiting){
+              return Text('Loading');
+            }
+            final data = snapshot.requireData;
+
+            /*int sIze = 1;
+            for(int i=0; i != data.size; i++){
+              if(data.docs[i]['user_uid'] == FirebaseAuth.instance.currentUser!.uid){
+                sIze+=1;
+              }
+              else{
+                continue;
+              }
+            }*/
+            return DataTable(
+                columns: [
+                  DataColumn(label: Text("Date&Time")),
+                  DataColumn(label: Text("Operation")),
+                  DataColumn(label: Text("Sum")),
+                  DataColumn(label: Text("Account")),
+                ],
+                rows: List.generate(data.size, (index) =>
+
+                    buildList(data, index,snapshot))
+            );
+          }),
+      /*DataTable(
+          columns: [
+            DataColumn(label: Text("Name")),
+            DataColumn(label: Text("Sum")),
+          ],
+          rows: [
+            DataRow(cells: [
+              DataCell(Text("Tinkoff")),
+              DataCell(Text("10000")),
+            ]),
+            DataRow(cells: [
+              DataCell(Text("Alpha")),
+              DataCell(Text("354")),
+            ]),
+          ],
+        )*/
+    );
+    /*DataTable(
       columns: [
         DataColumn(label: Text("Date&Time")),
         DataColumn(label: Text("Operation")),
@@ -88,7 +146,86 @@ class TableReportState extends State<TableReport>{
           DataCell(Text("Alpha")),
         ]),
       ],
-    );
+    );*/
 
   }
+  DataRow buildList(data, g, snapshot) {
+    var docs = snapshot.data.docs;
+    final acc = docs[g].data()!;
+    final f = new DateFormat('yyyy-MM-dd hh:mm');
+    //var date = DateTime.fromMicrosecondsSinceEpoch(miliseconds * 1000);
+   // DateFormat(DateFormat.YEAR_MONTH_DAY, 'pt_Br').format(date.toUtc());
+    /*List<String> wasd = <String>[];
+    for(int i=0; i < wasd.length; i++){
+      wasd.insert(i, acc['summary'].toString());
+    }*/
+    //print(wasd.runtimeType);
+    //print("${acc['name'].runtimeType}");
+    //wasd.clear();
+
+    //wasd.insert(wasd.length, acc['summary'].toString());
+    /*print(wasd.length);
+    print(wasd);*/
+    /*for(int i=0; i < wasd.length; i++){
+      print(wasd[i]);
+    }*/
+    return DataRow(cells: [
+//f.format(new DateTime.fromMillisecondsSinceEpoch(acc['date_time']*1000))
+    //${acc['date_time'].toDate()}
+      DataCell(Text("${acc['date_time'].toDate().toString().substring(0,16)}")),
+      DataCell(Text("${acc['operation']}")),
+      DataCell(Text("${acc['summary']}")),
+      DataCell(Text("${acc['account']}")),
+
+    ]);
+    /*if (acc['user_uid'] == FirebaseAuth.instance.currentUser!.uid){
+      return DataRow(cells: [
+      DataCell(Text(acc['name'])),
+      DataCell(Text("${acc['summary']}")),
+    ]);
+    }
+    else{
+      print('error');
+    }
+*/
+    /*return DataRow(
+        cells: [
+          DataCell(
+            Container(
+              width: 25,
+            )
+          ),
+          DataCell(
+              Container(
+                width: 25,
+              )
+          )
+        ]);*/
+
+    /*return DataRow(cells: [
+      DataCell(Text(
+          acc['user_uid'] == FirebaseAuth.instance.currentUser!.uid
+              ? acc['name']: null)
+      ),
+      DataCell(Text(
+          acc['user_uid'] == FirebaseAuth.instance.currentUser!.uid
+              ? "${acc['summary']}" : "")
+      ),
+    ]);*/
+    /*return DataRow(cells: [
+      DataCell(Text(
+          acc['user_uid'] == FirebaseAuth.instance.currentUser!.uid
+              ? acc['name']: "")
+      ),
+      DataCell(Text(
+          acc['user_uid'] == FirebaseAuth.instance.currentUser!.uid
+              ? "${acc['summary']}" : "")
+      ),
+    ]);*/
+    /*return DataRow(cells: [
+      DataCell(Text(data.docs[i]['name'])),
+      DataCell(Text("${data.docs[i]['summary']}")),
+    ]);*/
+  }
+
 }
